6c46d425cb56dfa38c26128fe0bad38dcbb32e4e
Jussi Aalto <jussi.aalto@cs.helsinki.fi>
2017-04-07 11:06:26 +0300

    tehty nelosviikkoa

--- koodi/viikko4/TyhjaProjekti/src/main/java/ohtu/Main.java
+++ koodi/viikko4/TyhjaProjekti/src/main/java/ohtu/Main.java
@@ -3,7 +3,6 @@ package ohtu;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonPrimitive;
 import java.io.IOException;
 import org.apache.http.client.fluent.Request;
 
