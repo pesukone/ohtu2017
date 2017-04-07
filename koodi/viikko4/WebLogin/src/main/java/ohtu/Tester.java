c4bb640a87e61dc9114a66c51844686bea5e7188
Jussi Aalto <jussi.aalto@cs.helsinki.fi>
2017-04-07 15:28:33 +0300

    tehty nelosviikkoa

--- koodi/viikko4/WebLogin/src/main/java/ohtu/Tester.java
+++ koodi/viikko4/WebLogin/src/main/java/ohtu/Tester.java
@@ -4,11 +4,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 public class Tester {
 
     public static void main(String[] args) {
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new HtmlUnitDriver();
 
         driver.get("http://localhost:4567");
         
@@ -29,6 +30,12 @@ public class Tester {
         element.submit();
 
         sleep(3);
+		
+		login(driver, "pekka", "väärä");
+		login(driver, "olematon", "mikälie");
+		
+		createUser(driver, "make", "install");
+		logoutAfterCreation(driver);
         
         driver.quit();
     }
@@ -38,4 +45,45 @@ public class Tester {
             Thread.sleep(n*1000);
         } catch(Exception e){}
     }
+	
+	private static void login(WebDriver driver, String username, String password) {
+		driver.get("http://localhost:4567");
+		
+		WebElement element = driver.findElement(By.linkText("login"));
+		element.click();
+		
+		element = driver.findElement(By.name("username"));
+		element.sendKeys(username);
+		
+		element = driver.findElement(By.name("password"));
+		element.sendKeys(password);
+		
+		element = driver.findElement(By.name("login"));
+		element.click();
+	}
+	
+	private static void createUser(WebDriver driver, String username, String password) {
+		driver.get("http://localhost:4567");
+		
+		WebElement element = driver.findElement(By.linkText("register new user"));
+		element.click();
+		
+		element = driver.findElement(By.name("username"));
+		element.sendKeys(username);
+		
+		element = driver.findElement(By.name("password"));
+		element.sendKeys(password);
+		
+		element = driver.findElement(By.name("passwordConfirmation"));
+		element.sendKeys(password);
+		
+		element = driver.findElement(By.name("signup"));
+		element.click();
+	}
+	
+	public static void logoutAfterCreation(WebDriver driver) {
+		System.out.println(driver.getPageSource());
+		WebElement element = driver.findElement(By.linkText("continue to application mainpage"));
+		element.click();
+	}
 }
