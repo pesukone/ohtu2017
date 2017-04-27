d963d0b7396f0de9f7504fc4671ba105ae5d9f21
Jussi Aalto <jussi.aalto@cs.helsinki.fi>
2017-04-27 10:36:25 +0300

    tehty kutosviikkoa

--- koodi/viikko6/Laskin/src/main/java/ohtu/Nollaa.java
+++ koodi/viikko6/Laskin/src/main/java/ohtu/Nollaa.java
@@ -5,21 +5,24 @@ import javax.swing.JTextField;
 public class Nollaa implements Komento {
 	private Sovelluslogiikka sovellus;
 	private JTextField tuloskentta;
-	private String edellinenTulos;
+	private int edellinenTulos;
 	
 	public Nollaa(Sovelluslogiikka sovellus, JTextField tuloskentta) {
+		this.sovellus = sovellus;
 		this.tuloskentta = tuloskentta;
-		this.edellinenTulos = tuloskentta.getText();
+		this.edellinenTulos = 0;
 	}
 	
 	@Override
 	public void suorita() {
+		this.edellinenTulos = this.sovellus.tulos();
 		this.sovellus.nollaa();
 		this.tuloskentta.setText("0");
 	}
 	
 	@Override
 	public void peru() {
-		this.tuloskentta.setText(this.edellinenTulos);
+		this.sovellus.plus(this.edellinenTulos);
+		this.tuloskentta.setText("" + this.sovellus.tulos());
 	}
 }
